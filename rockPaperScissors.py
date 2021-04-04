9f1a6d782a819f7418bff3d26d2f8ed2e5d0a205
EduardoUrbiola <51526562+EduardoUrbiola@users.noreply.github.com>
2021-04-03 20:55:00 -0600

    Add files via upload
    Rock, Paper, Scissors Game!

---
+++ rockPaperScissors.py
@@ -0,0 +1,54 @@
+# Rock, Paper, Scissors Game!
+from random import randint
+
+rock = '🪨'
+paper = '📄'
+scissors = '✂️'
+
+
+player = input('\n🪨, 📄 or ✂️ ? ').capitalize()
+random_number = randint(0,2)
+
+if player == 'Rock':
+	player = rock
+elif player == 'Paper':
+	player = paper
+else: 
+	player = scissors
+print(f'\nPlayer chose {player}')
+
+if random_number == 0:
+	computer = rock
+elif random_number == 1:
+	computer = paper
+else: 
+	computer = scissors
+print(f'Computer chose {computer}\n')
+
+if player == computer:
+	print('It\'s a tie\n')
+elif player == rock:
+	if computer == paper:
+		print('Player wins!\n')
+	else:
+		print('Computer wins!\n')
+elif player == paper:
+	if computer == rock:
+		print('Player wins!\n')
+	else:
+		print('Computer wins!\n')
+elif player == scissors:
+	if computer == paper:
+		print('Player wins!\n')
+	else:
+		print('Computer wins!\n')
+else:
+	print('Please write a valid item\n')
+
+
+
+
+
+
+
+
