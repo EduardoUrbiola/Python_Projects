757b228425c7931414898536b6ddd5ed19849518
EduardoUrbiola <51526562+EduardoUrbiola@users.noreply.github.com>
2021-04-03 22:02:47 -0600

    Create rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -11,25 +11,26 @@ print('\nWelcome to the 🪨, 📄, ✂️  game!')
 player = input('\nChoose 🪨, 📄 or ✂️ ? ').capitalize()
 random_number = randint(0,2)
 
+
 if player == 'Rock':
 	player = rock
 elif player == 'Paper':
 	player = paper
 elif player == 'Scissors':
 	player = scissors
-elif player != 'Rock' or 'Paper' or 'Scissors':
+else:
 	player = no_response
+
 print(f'\nPlayer chose {player}')
 
-if random_number >= 0 or random_number <= 2 and player != 'Rock' or 'Paper' or 'Scissors':
-	computer = unable_to_play
+if player == no_response:
+	computer = unable_to_play 
 elif random_number == 0:
 	computer = rock
 elif random_number == 1:
 	computer = paper
 elif random_number == 2:
 	computer = scissors
-
 print(f'Computer chose {computer}\n')
 
 if player == computer:
